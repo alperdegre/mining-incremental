5d6921efa00345664e0b915e3ad13004f01ba6bd
Alper Değre <alper.degre.dev@gmail.com>
2022-07-16 22:22:32 +0300

    Moved initial miners to a .json file

--- src/features/miners/minersSlice.js
+++ src/features/miners/minersSlice.js
@@ -1,60 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import initialState from "../../data/miners.json";
 
 export const minersSlice = createSlice({
   name: "miners",
-  initialState: {
-    miners: [
-      {
-        id: 0,
-        name: "Stone",
-        amount: 0,
-        perSecond: 0,
-        perMiner: 0.1,
-        cost: 10,
-        unlocked: true,
-      },
-      {
-        id: 1,
-        name: "Iron",
-        amount: 0,
-        perSecond: 0,
-        perMiner: 1,
-        cost: 100,
-        unlocked: false,
-      },
-      {
-        id: 2,
-        name: "Gold",
-        amount: 0,
-        perSecond: 0,
-        perMiner: 10,
-        cost: 1000,
-        unlocked: false,
-      },
-      {
-        id: 3,
-        name: "Emerald",
-        amount: 0,
-        perSecond: 0,
-        perMiner: 100,
-        cost: 10000,
-        unlocked: false,
-      },
-      {
-        id: 4,
-        name: "Diamond",
-        amount: 0,
-        perSecond: 0,
-        perMiner: 1000,
-        cost: 100000,
-        unlocked: false,
-      },
-    ],
-    unlocks: {
-      unlockTresholds: [100, 1000, 10000, 100000],
-      unlockProgress: 0,
-    },
-  },
+  initialState : initialState,
   reducers: {
     buyOne: (state, action) => {
       state.miners[action.payload].amount += 1;
@@ -66,7 +15,7 @@ export const minersSlice = createSlice({
     buyUntil10: (state, action) => {
       const amountLeftUntil10 = 10 - (state.miners[action.payload].amount % 10);
       state.miners[action.payload].amount += amountLeftUntil10;
-      state.miners[action.payload].perSecond = +(
+      state.miners[action.payload].perSecond += +(
         amountLeftUntil10 * state.miners[action.payload].perMiner
       ).toFixed(2);
     },
